b14e7dc67b87425327a354c2ac21b3768c023ccf
Mart <marta.nikiel@yahoo.com>
2024-02-28 10:14:26 +0100

    added verifyEmail

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,9 +1,11 @@
 const getCurrent = require("./getCurrent");
 const updateAvatar = require("./updateAvatar");
 const resendVerifyEmail = require("./resendVerifyEmail");
+const verifyEmail = require("./verifyEmail");
 
 module.exports = {
   getCurrent,
   updateAvatar,
   resendVerifyEmail,
+  verifyEmail,
 };
